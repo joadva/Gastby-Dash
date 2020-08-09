f6771f601e09d78eafc46009cd190d3691f67fda
adrian <joadva@hotmail.com>
2020-08-09 17:03:41 -0500

    Login ok

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,17 +1,14 @@
 import React from "react"
 import { Router } from "@reach/router"
-import Layout from "../components/layout"
 import Perfil from "../components/perfil"
 import Login from "../components/login"
 import RutaPrivada from "../components/rutaPrivada"
 
 export default function App() {
   return (
-    <Layout>
-      <Router>
-        <RutaPrivada path="/app/perfil" component={Perfil} />
-        <Login path="/app/login" />
-      </Router>
-    </Layout>
+    <Router>
+      <RutaPrivada path="/app/perfil" component={Perfil} />
+      <Login path="/app/login" />
+    </Router>
   )
 }
